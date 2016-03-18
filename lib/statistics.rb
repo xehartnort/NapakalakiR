f8c9416e0d313e66567282e9b563ec7a82d41f0e
Daniel Jiménez López <xehartnort@gmail.com>
2016-03-18 18:20:56 +0100

    Update statistics.rb
    Unas correcciones por haber leído mal el examen

--- lib/statistics.rb
+++ lib/statistics.rb
@@ -1,23 +1,22 @@
-#encoding: utf-8
-# and open the template in the editor.
+
 module EX2
   
   class Statistics
   
   def compute(marray)
     nivelmax=0
-    media_tesoros=0
+    @media_tesoros=0
     #Calcular el nombre del monstruo con el nivel máximo
     # Calcula la media de los tesoros que proporciona cada mosntruo
     marray.each { |i|
       if i.level > nivelmax
         nivelmax= i.level
-        nombre_nivel_max = i.name
+        @nombre_nivel_max = i.name
       end
-      media_tesoros += i.prize.treasures
+      @media_tesoros += i.prize.treasures
     
     }
-    media_tesoros = media_tesoros / marray.length 
+    @media_tesoros = media_tesoros / marray.length 
     
     marray.each { |i|
         #Monstruos que quitan tesoros ocultos no específicos
