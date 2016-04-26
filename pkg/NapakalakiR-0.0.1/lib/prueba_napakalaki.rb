19188c9ad40bbd2e36ebdd2ce76bad8450d2b9b7
Daniel Jimenez <xehartnort@gmail.com>
2016-04-27 01:09:56 +0200

    Todo funcionando, a excepción de discardAllTreasures

---
+++ pkg/NapakalakiR-0.0.1/lib/prueba_napakalaki.rb
@@ -0,0 +1,8 @@
+#encoding: utf-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+require_relative 'monster.rb'
+require_relative 'player.rb'
+
